fb496fa32c86dc8635482f92840a6cb213e7bbcb
Will H McMahan <will@thoughtbot.com>
2016-08-28 13:18:01 -0400

    Add middleman

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+ruby "2.3.1"
+
+gem "bourbon", "5.0.0.beta.6"
+gem "middleman", ">= 4.0.0"
+gem "middleman-autoprefixer"
+gem "middleman-livereload"
+gem "middleman-minify-html"
+gem "neat", "2.0.0.beta.1"
+gem "redcarpet"
+gem "slim"
---
+++ config.rb
@@ -0,0 +1,34 @@
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
+
+set :css_dir, "assets/stylesheets"
+set :images_dir, "assets/images"
+set :js_dir, "assets/javascripts"
+set :markdown_engine, :redcarpet
+set :markdown,
+  autolink: true,
+  fenced_code_blocks: true,
+  footnotes: true,
+  highlight: true,
+  smartypants: true,
+  strikethrough: true,
+  tables: true,
+  with_toc_data: true
+
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
+
+configure :development do
+  activate :livereload do |reload|
+    reload.no_swf = true
+  end
+end
+
+configure :build do
+  activate :asset_hash
+  activate :gzip
+  activate :minify_css
+  activate :minify_html
+end
